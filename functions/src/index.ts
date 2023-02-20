f04da9e4f81a97f26c4cb55297bbcb77418cbe25
Nico Enking <nico.enking@gmail.com>
2023-02-20 14:05:47 +0100

    Use Caching for storage

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -1,6 +1,7 @@
 import * as functions from "firebase-functions";
 import * as admin from "firebase-admin";
 import tts from "@google-cloud/text-to-speech";
+import * as hash from "object-hash";
 
 admin.initializeApp();
 
@@ -12,23 +13,45 @@ export const ttsGen = functions
     const ttsClient = new tts.TextToSpeechClient();
 
     const data = snapshot.data();
+    const ttsText = data.ttsText;
+    const ttsTextHash = hash(ttsText);
 
-    const [response] = await ttsClient.synthesizeSpeech({
-      audioConfig: {
-        audioEncoding: 'MP3',
-      },
-      input: {
-        text: data.ttsText,
-      },
-      voice: {
-        languageCode: 'de-DE',
-        name: 'de-DE-Neural2-D',
+    const ttsBucket = admin.storage().bucket();
+    const bucketPath = `alerts/${ttsTextHash}.mp3`;
+
+    // Check if bucket already has the text as speech synthesis
+    const file = ttsBucket.file(bucketPath);
+    const fileExists = await file.exists();
+    if (fileExists[0]) {
+      await snapshot.ref.update({
+        ...data,
+        bucketPath,
+      });
+    } else {
+      const [response] = await ttsClient.synthesizeSpeech({
+        audioConfig: {
+          audioEncoding: 'MP3',
+        },
+        input: {
+          text: ttsText,
+        },
+        voice: {
+          languageCode: 'de-DE',
+          name: 'de-DE-Neural2-D',
+        }
+      });
+  
+      const audioContent = response.audioContent;
+  
+      if (audioContent === undefined || audioContent === null) {
+        throw new Error(`audioContent for document ${snapshot.id} was undefined for ttsText ${ttsText}`);
       }
-    });
-
-    console.log(response);
-
-    // const ttsBucket = admin.storage().bucket();
-
-    // await ttsBucket.file(`alerts/${snapshot.id}.mp3`).save(response.audioContent);
+  
+      await ttsBucket.file(bucketPath).save(audioContent.toString());
+  
+      await snapshot.ref.update({
+        ...data,
+        bucketPath,
+      });
+    }
   });
