bd8dc2fd3ebeab9bad1547fb0bd3b074d7494f61
Nico Enking <nico.enking@gmail.com>
2023-02-21 22:16:50 +0100

    Refactor index page

--- app/src/pages/index.tsx
+++ app/src/pages/index.tsx
@@ -1,7 +1,11 @@
-import { Title } from '@mantine/core';
+import { Flex, Title } from '@mantine/core';
+import logo from '../assets/sam-logo.svg';
 
 export default function IndexPage() {
   return (
-    <Title>Startseite</Title>
+    <Flex direction="column" h="100%" align="center" justify="center">
+      <img src={logo} width="30%" height="30%" />
+      <Title mt="md">SAM Simulator</Title>
+    </Flex>
   )
 }
