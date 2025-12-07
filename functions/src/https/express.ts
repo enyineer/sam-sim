b45dde2f798db76bd113888b0d62f4c4a914538f
enyineer <nico.enking@gmail.com>
2025-12-07 09:33:53 +0100

    Add types, skip lib check
    Signed-off-by: enyineer <nico.enking@gmail.com>

--- functions/src/https/express.ts
+++ functions/src/https/express.ts
@@ -1,16 +1,16 @@
 import * as express from "express";
 import * as cors from "cors";
-import * as trpcExpress from '@trpc/server/adapters/express';
-import { appRouter } from './trpc/server';
-import { createContext } from './trpc/context';
+import * as trpcExpress from "@trpc/server/adapters/express";
+import { appRouter } from "./trpc/server";
+import { createContext } from "./trpc/context";
 
 export const app = express();
 
 app.use(cors({ origin: true }));
 app.use(
-  '/',
+  "/",
   trpcExpress.createExpressMiddleware({
     router: appRouter,
     createContext: createContext,
-  }),
+  })
 );
